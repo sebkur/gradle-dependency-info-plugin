47913d77fe92f32ce7ec42c8fe95bea9b45dce51
Sebastian Kürten <sebastian@topobyte.de>
2024-12-29 21:47:58 +0100

    Add testing projects

---
+++ test-local/test.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+pushd .
+cd test1
+pwd
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew clean test createRuntime
+./scripts/Test
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew depinfo
+popd
+
+pushd .
+cd test2
+pwd
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew clean test createRuntime
+./scripts/Test
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew depinfo
+popd
+
+pushd .
+cd test2
+pwd
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/ ./gradlew clean test createRuntime
+./scripts/Test
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/ ./gradlew depinfo
+popd
+
+pushd .
+cd test3
+pwd
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew clean test createRuntime
+./scripts/Test
+JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ ./gradlew depinfo
+popd
+
+pushd .
+cd test3
+pwd
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/ ./gradlew clean test createRuntime
+./scripts/Test
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/ ./gradlew depinfo
+popd
